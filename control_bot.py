d7ce64eee0f36c91ae1ce96ac466257b879eebd6
DIx37 <dkarnov@gmail.com>
2021-08-25 11:15:35 +0300

    v2.50

--- control_bot.py
+++ control_bot.py
@@ -1,7 +1,10 @@
 import os
-
+#system.run["service DeliveryBot status | grep -v grep | grep 'running' | wc -l"]
+#def status(proc):
+#    status = os.popen(f"systemctl status {proc}.service").read()
+#    return status
 def status(proc):
-    status = os.popen(f"systemctl status {proc}.service").read()
+    status = os.popen(f"service {proc} status | grep -v grep | grep 'running' | wc -l").read()
     return status
 
 def stop(proc):
